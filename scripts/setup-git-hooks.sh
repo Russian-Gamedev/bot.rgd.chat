8a4700ecef8db2c09312a21c0101cc4b031009a8
DamirLut <lutfrahmanovdamir3@gmail.com>
2026-04-03 12:13:32 +0500

    feat:  update hooks path in setup script

--- scripts/setup-git-hooks.sh
+++ scripts/setup-git-hooks.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 if git rev-parse --git-dir > /dev/null; then
-  git config core.hooksPath .githooks
+  git config core.hooksPath .github/hooks
 else
   echo "Error: This script must be run inside a Git repository."
 fi
