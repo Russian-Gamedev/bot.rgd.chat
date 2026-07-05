02fa87a42a7a7e73fd541b0cbd8f046bd8288ceb
DamirLut <lutfrahmanovdamir3@gmail.com>
2026-07-05 14:23:44 +0500

    refactor: remove shiz vote game and related config

--- src/core/mini-games/mini-games.module.ts
+++ src/core/mini-games/mini-games.module.ts
@@ -5,11 +5,10 @@ import { UserModule } from '#core/users/users.module';
 import { WalletModule } from '#core/wallet/wallet.module';
 
 import { FlipGame } from './games/flip.game';
-import { RandomMuteGame } from './games/random-mute.game';
 import { SlotGame } from './games/slot.game';
 
 @Module({
   imports: [UserModule, WalletModule, GuildSettingsModule],
-  providers: [FlipGame, SlotGame, RandomMuteGame],
+  providers: [FlipGame, SlotGame],
 })
 export class MiniGamesModule {}
